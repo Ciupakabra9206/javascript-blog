8bb1a02b19092b2f06ce68ef2ba55c593fd9111b
Paweł Prorok <pawel3474@tlen.pl>
2019-12-18 00:40:56 +0100

    Add first funcion

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  });*/
+  const titleClickHandler = function(){
+    console.log('Link was clicked!');
+
+  const links = document.querySelectorAll('.titles a');
+
+   /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+ for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
